e83b761a0e0186966fdba9ec596431122d545ad4
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-07-07 18:05:05 -0400

    Add .get from etsy api

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -3,9 +3,10 @@ app.controller("homepageController", ["$http", "$scope", function($http, $scope)
     $scope.message = "angular!";
     console.log("it works!");
 
-    $http.get("http//localhost:3007/db").success(function(data){
+    $http.get("https://openapi.etsy.com/v2/taxonomy/buyer/get/?api_key=nsh3xdn1xlab8cak8wsrh8f6").success(function(data){
       console.log("it's talking to the json.");
       $scope.content = data;
+      console.log($scope.content);
 
 
     });
