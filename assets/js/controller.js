f6f2a4a2451f7ce7a63adb61fb03aaf554f057c8
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-07-08 17:17:25 -0400

    Add commit

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -5,8 +5,10 @@ console.log("Hello!");
     console.log("it works!");
     $http.get("https://openapi.etsy.com/v2/listings/active?api_key=nsh3xdn1xlab8cak8wsrh8f6&keywords=whiskey&includes=Images,Shop&limit=12&offset=0").then(function(response){
       console.log("it's talking to the json.");
-      $scope.info = response.data;
-      console.log($scope.info.results[0].Images[0].url_fullxfull);
+      $scope.info = response.data.results;
+      // console.log($scope.info);
+      console.log($scope.info);
+      // var images
     });
     var contentID;
     $scope.showProdID = function(contentNum){
