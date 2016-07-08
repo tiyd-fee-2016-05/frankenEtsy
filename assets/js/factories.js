526c1c7ac53808a62bd5bae0ca4eeebe621565d3
Jordan Long <jordan.long222@gmail.com>
2016-07-08 10:59:45 -0400

    fix API call variable for Images instead of MainImage since it was returning an error

--- assets/js/factories.js
+++ assets/js/factories.js
@@ -3,7 +3,7 @@ mainApp.factory('trendingFactory', ['$http', '$scope', function() {
 
     var apiKEY = "api_key=9l86gozghbufirehm13mtdky";
     var baseURL = "https://openapi.etsy.com/v2/listings/trending?";
-    var variations = "includes=Shop,MainImage&";
+    var variations = "includes=Shop,Images&";
 
     trendingFactory.getTrends = function() {
         return $http.get(baseURL + apiKEY);
