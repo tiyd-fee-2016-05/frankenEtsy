0838126768dd32a0b7568a14a3dcdb434f125899
Jordan Long <jordan.long222@gmail.com>
2016-07-08 10:54:38 -0400

    add factory / controller / JSON edits, and img

---
+++ assets/js/factories.js
@@ -0,0 +1,14 @@
+mainApp.factory('trendingFactory', ['$http', '$scope', function() {
+    //NEED TO RETURN API INFORMATION since factories need to return a value
+
+    var apiKEY = "api_key=9l86gozghbufirehm13mtdky";
+    var baseURL = "https://openapi.etsy.com/v2/listings/trending?";
+    var variations = "includes=Shop,MainImage&";
+
+    trendingFactory.getTrends = function() {
+        return $http.get(baseURL + apiKEY);
+    };
+
+    return trendingFactory;
+
+}]);
