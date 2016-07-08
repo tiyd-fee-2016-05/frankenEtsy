db0377ccf42337835962c50ad23fd23d677be4a4
SINOVIA <somayfield@gmail.com>
2016-07-08 15:46:31 -0400

    Updated JSON call

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -3,14 +3,15 @@ etsyApp.controller("homepageController", ["$http", "$scope", function($http, $sc
 console.log("Hello!");
     $scope.message = "Whoever you are, find";
     console.log("it works!");
-    $http.get("https://openapi.etsy.com/v2/listings/trending?includes=Shop,Images&api_key=nsh3xdn1xlab8cak8wsrh8f6").success(function(data){
+    $http.get("https://openapi.etsy.com/v2/listings/active?api_key=nsh3xdn1xlab8cak8wsrh8f6&keywords=whiskey&includes=Images,Shop&limit=12&offset=0").then(function(response){
       console.log("it's talking to the json.");
-      $scope.content = data;
-      console.log($scope.content);
+      $scope.info = response.data;
+      console.log($scope.info.results[0].Images[0].url_fullxfull);
     });
     var contentID;
     $scope.showProdID = function(contentNum){
       contentID = contentNum;
       console.log("contentId =" + contentId);
     };
+
 }]);
