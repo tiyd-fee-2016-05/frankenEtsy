bb7ffb920055702903e1e682bb963f335460320e
David <dgrayboff@gmail.com>
2016-07-09 10:07:46 -0400

    initial commit

---
+++ assets/js/productController.js
@@ -0,0 +1,116 @@
+mainApp.controller('productCtrl', function($scope, $http) {
+  $http({
+    method: 'GET',
+    url: 'https://openapi.etsy.com/v2/listings/'+ 175112598 +'?api_key=9tojngbsrbfw4ezg2zfrrqvo'
+  }).then(function successCallback(response){
+    $scope.product = response.data;
+    $scope.desc = JSON.stringify(response.data.results[0].description);
+    $scope.price = response.data.results[0].price;
+    $scope.title = response.data.results[0].title;
+    $scope.views = response.data.results[0].views;
+    $scope.favorers = response.data.results[0].num_favorers;
+    // for (var i = 0; i < response.data.results[0].materials.length; i++) {
+    // console.log(i);
+    $scope.materials = response.data.results[0].materials;
+    $scope.material= $scope.materials.toString();
+    $scope.listedOn = response.data.results[0].last_modified_tsz;
+    $scope.tPath = response.data.results[0].taxonomy_path;
+    $scope.tags = response.data.results[0].tags;
+    $scope.quantity = response.data.results[0].quantity;
+    // }
+    console.log($scope.desc);
+    console.log($scope.product);
+    console.log($scope.materials);
+    console.log("Quantity:" + $scope.quantity);
+
+
+      // $scope.oneOption = true;
+      if  ($scope.quantity === 1) {
+        $scope.availOptions = false;
+        $scope.oneOption = true;
+      }
+      else {
+        $scope.quantOptions =  [];
+        for (var x = 1; x <= $scope.quantity; x++){
+          $scope.quantOptions.push(x);
+          console.log($scope.quantOptions)
+        }
+        $scope.availOptions = true;
+        $scope.oneOption = false;
+      }
+
+    console.log("show that ish");
+  });
+
+  $scope.deatsTab = true;
+  $scope.reviewTab= false;
+  $scope.shipTab= false;
+
+  $scope.reviewTabFunc= function(){
+    $scope.deatsTab = false;
+    $scope.reviewTab= true;
+    $scope.shipTab= false;
+  }
+  $scope.deatsTabFunc= function(){
+    $scope.deatsTab = true;
+    $scope.reviewTab= false;
+    $scope.shipTab= false;
+  }
+  $scope.shipTabFunc= function(){
+    $scope.deatsTab = false;
+    $scope.reviewTab= false;
+    $scope.shipTab= true;
+  }
+
+
+  $http({
+    method: 'GET',
+    url: 'https://openapi.etsy.com/v2/listings/'+ 175112598 +'/images/?api_key=9tojngbsrbfw4ezg2zfrrqvo'
+    }).then(function imageCallBack(response) {
+      console.log(response);
+      $scope.thumbnail = [];
+      $scope.loadImage = response.data.results[0].url_570xN;
+      for(var y= 0; y < response.data.results.length; y++){
+        $scope.thumbnail.push(response.data.results[y].url_75x75)
+        console.log($scope.thumbnail);
+      }
+      var imgCount = 0;
+      $scope.imgFuncUp = function(){
+        imgCount++;
+        if(imgCount <= response.data.results.length - 1){
+          $scope.loadImage = response.data.results[imgCount].url_570xN;
+          // console.log(imgCount);
+          // console.log(response.data.results.length)
+        }
+        if (imgCount === response.data.results.length) {
+          imgCount = 0;
+          $scope.loadImage = response.data.results[imgCount].url_570xN;
+          // console.log(imgCount);
+        }
+      }
+      $scope.imgFuncDown = function(){
+        imgCount--
+        // console.log(imgCount)
+        if(imgCount < 0){
+          imgCount=4;
+          // console.log(imgCount);
+        }
+        if (imgCount >= 0){
+          $scope.loadImage = response.data.results[imgCount].url_570xN;
+        }
+
+      }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+});
