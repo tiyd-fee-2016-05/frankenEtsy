ec781242403a7f532e0bc9c36bb24872c194906e
Travis Johnson <trav.johnson919@gmail.com>
2016-07-09 15:49:41 -0400

    almost there with json stuff

--- assets/js/categoriesController.js
+++ assets/js/categoriesController.js
@@ -3,14 +3,33 @@
 mainApp.controller( "CategoriesController", [ "$scope", "dataFactory", function( $scope, dataFactory ) {
 
   $scope.listings; // used to store data for "Shop all items" section
-  $scope.jewelry;
-  $scope.home;
-  $scope.shops;
+  // $scope.jewelry;
+  // $scope.home;
+  $scope.necklaces;
+  $scope.decor;
+  $scope.beading;
+  $scope.Partysupplies;
+  $scope.Womensclothing;
+  $scope.prints;
+  $scope.earrings;
+  // $scope.prints;
+  // $scope.toys;
+  // $scope.books;
+
   var numGets = 0; // count to see if factory is caching data or not
 
   getListings();
-  getJewelry();
-  getHome();
+  // getJewelry();
+  // getHome();
+  getNecklaces();
+  getDecor();
+  getBeading();
+  getPartysupplies();
+  getWomensclothing();
+  getPrints();
+  getEarrings();
+  // getPrints();
+  // getBooks();
 
   // function to get items for "Shop all items"
   function getListings() {
@@ -32,29 +51,111 @@ mainApp.controller( "CategoriesController", [ "$scope", "dataFactory", function(
   } // end getListings()
 
   // function to get jewelry
-  function getJewelry() {
-    dataFactory.getJewelry()
-      .then( function( response ) {
-        $scope.jewelry = response.data.results[0].Images[0].url_75x75;
-        console.log( response.data.results[0].Images[0].url_75x75 );
-        // console.log("jewelry pic " + $scope.jewelry.results );
-      },
-      function( error ) {
-        console.log( "Denied!!! " + error.message );
-      });
-  } // end getJewelry()
+  // function getJewelry() {
+  //   dataFactory.getJewelry()
+  //     .then( function( response ) {
+  //       $scope.jewelry = response.data.results[0].Images[0].url_fullxfull;
+  //       console.log( response.data.results[0].Images[0].url_fullxfull );
+  //       // console.log("jewelry pic " + $scope.jewelry.results );
+  //     },
+  //     function( error ) {
+  //       console.log( "Denied!!! " + error.message );
+  //     });
+  // } // end getJewelry()
 
   // function to get home
-  function getHome() {
-    dataFactory.getHome()
+  // function getHome() {
+  //   dataFactory.getHome()
+  //     .then( function( response ) {
+  //       $scope.home = response.data.results[0].Images[0].url_fullxfull;
+  //       console.log( response.data.results[0].Images[0].url_fullxfull  );
+  //     },
+  //     function( error ) {
+  //       console.log( "Denied!!! " + error.message );
+  //     });
+  // } // end gethome()
+// function to get Necklaces
+function getNecklaces() {
+  dataFactory.getNecklaces()
+    .then( function( response ) {
+      $scope.necklaces = response.data.results[0].Images[0].url_fullxfull;
+      console.log( response.data.results[0].Images[0].url_fullxfull  );
+    },
+    function( error ) {
+      console.log( "Denied!!! " + error.message );
+    });
+  }
+// // end getNecklaces
+// function to getDecor
+function getDecor() {
+  dataFactory.getDecor()
+    .then( function( response ) {
+      $scope.decor = response.data.results[0].Images[0].url_fullxfull;
+      console.log( response.data.results[0].Images[0].url_fullxfull  );
+    },
+    function( error ) {
+      console.log( "Denied!!! " + error.message );
+    });
+  }
+// end getDecor
+// function to get beading
+function getBeading() {
+  dataFactory.getBeading()
+    .then( function( response ) {
+      $scope.beading = response.data.results[0].Images[0].url_fullxfull;
+      console.log( response.data.results[0].Images[0].url_fullxfull  );
+    },
+    function( error ) {
+      console.log( "Denied!!! " + error.message );
+    });
+  }
+
+function getPartysupplies() {
+  dataFactory.getPartysupplies()
+    .then( function( response ) {
+      $scope.Partysupplies = response.data.results[0].Images[0].url_fullxfull;
+      console.log( response.data.results[0].Images[0].url_fullxfull  );
+    },
+    function( error ) {
+      console.log( "Denied!!! " + error.message );
+    });
+  }
+// // end getPaper
+function getWomensclothing() {
+  dataFactory.getWomensclothing()
+    .then( function( response ) {
+      $scope.Womensclothing = response.data.results[0].Images[0].url_fullxfull;
+      console.log( response.data.results[0].Images[0].url_fullxfull  );
+    },
+    function( error ) {
+      console.log( "Denied!!! " + error.message );
+    });
+  }
+
+// function to getEarrings
+function getEarrings() {
+  dataFactory.getEarrings()
+    .then( function( response ) {
+      $scope.earrings = response.data.results[0].Images[0].url_fullxfull;
+      console.log( response.data.results[0].Images[0].url_fullxfull  );
+    },
+    function( error ) {
+      console.log( "Denied!!! " + error.message );
+    });
+  }
+// // end getEarrings
+  // function to getPrints
+  function getPrints() {
+    dataFactory.getPrints()
       .then( function( response ) {
-        $scope.home = response;
-        console.log( $scope.home.results );
+        $scope.prints = response.data.results[0].Images[0].url_fullxfull;
+        console.log( response.data.results[0].Images[0].url_fullxfull  );
       },
       function( error ) {
         console.log( "Denied!!! " + error.message );
       });
-  } // end gethome()
+    }
+// // end getPrints
 
 
   $scope.goToProduct = function( listing ) {
@@ -62,7 +163,7 @@ mainApp.controller( "CategoriesController", [ "$scope", "dataFactory", function(
         console.log( JSON.stringify($scope.listings[$scope.productClicked]) );
         localStorage.setItem( "productClicked", JSON.stringify($scope.listings[$scope.productClicked]));
 
-  } // end goToProduct()
+  }; // end goToProduct()
 
 
 }]); // end mainApp.controller( "CategoriesController" );
