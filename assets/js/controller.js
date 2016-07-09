aecbc67c509f010a3098fce2a306509727ad6d04
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-07-09 14:20:00 -0400

    Add four api calls in total. Function for managing button clicks

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,52 +1,88 @@
 mainApp.controller('trendingController', ['$http', '$scope', function($http, $scope) {
 
-//FIRST CALL
-            //https://openapi.etsy.com/v2/listings/trending?limit=40&offset=0&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky
+    //FIRST CALL
+    //https://openapi.etsy.com/v2/listings/trending?limit=40&offset=0&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky
 
-//SECOND CALL
-            //https://openapi.etsy.com/v2/listings/trending?limit=40&offset=40&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky
+    //SECOND CALL
+    //https://openapi.etsy.com/v2/listings/trending?limit=40&offset=40&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky
 
-//THIRD CALL
-            //https://openapi.etsy.com/v2/listings/trending?limit=40&offset=80&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky
+    //THIRD CALL
+    //https://openapi.etsy.com/v2/listings/trending?limit=40&offset=80&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky
 
-//FOURTH CALL
-            //https://openapi.etsy.com/v2/listings/trending?limit=40&offset=120&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky
+    //FOURTH CALL
+    //https://openapi.etsy.com/v2/listings/trending?limit=40&offset=120&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky
 
 
     $http({
-      method: 'GET',
-      url: 'https://openapi.etsy.com/v2/listings/trending?limit=40&offset=0&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky'
-    }).then(function firstCall(first){
+        method: 'GET',
+        url: 'https://openapi.etsy.com/v2/listings/trending?limit=40&offset=0&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky'
+    }).then(function firstCall(first) {
         $scope.callOne = first.data.results;
-        console.log(first);                     //returns the entire response as an object
+        console.log(first); //returns the entire response as an object
 
-        first40 = first.data.results;          //first40 now is assigned the ARRAY OF OBJECTS (aka first 40 results)
+        first40 = first.data.results; //first40 now is assigned the ARRAY OF OBJECTS (aka first 40 results)
         console.log(first40);
 
     });
 
-    //fourth api call. triggered by button.
-    $('#loadMore').click(function(){
-      // e.preventDefault;
-      $http({
-        method: 'GET',
-        url: 'https://openapi.etsy.com/v2/listings/trending?limit=40&offset=120&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky'
-      }).then(function fourthCall (fourth){
-        $scope.callFour = fourth.data.results;
-        console.log(fourth);
 
-        // last40 = fourth.data.result;
-        // console.log(last40);
-      });
+    //second api call. triggered by button as of saturday, 7/9
+    var count = 0;
+
+    $('#loadMore').click(function(e) {
+      e.preventDefault();
+      count++;
+      lazyLoad(count);
+      console.log(count);
     });
 
-  //   if($(window).scrollTop()){
-  //   ($(document).height() - $(window).height()) * 0.7;
-  // }
-  //
-  // else{
-  //
-  // }
+
+    function lazyLoad(count){
+
+        switch (count) {
+
+
+            case 1:
+              console.log(count);
+                $http({
+                    method: 'GET',
+                    url: 'https://openapi.etsy.com/v2/listings/trending?limit=40&offset=40&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky'
+                }).then(function secondCall(second) {
+                    $scope.callTwo = second.data.results;
+                    console.log(second);
+                });
+                break;
+            case 2:
+            console.log(count);
+
+                $http({
+                    method: 'GET',
+                    url: 'https://openapi.etsy.com/v2/listings/trending?limit=40&offset=80&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky'
+                }).then(function thirdCall(third) {
+                    $scope.callThree = third.data.results;
+                    console.log(third);
+                });
+                break;
+            case 3:
+            console.log(count);
+
+                $http({
+                    method: 'GET',
+                    url: 'https://openapi.etsy.com/v2/listings/trending?limit=40&offset=120&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky'
+                }).then(function fourthCall(fourth) {
+                    $scope.callFour = fourth.data.results;
+                    console.log(fourth);
+                });
+                break;
+        };
+};
+    //   if($(window).scrollTop()){
+    //   ($(document).height() - $(window).height()) * 0.7;
+    // }
+    //
+    // else{
+    //
+    // }
 
 
 }]);
