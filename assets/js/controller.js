0e8498a6de199c9cbcad7e687c078c45a18770b9
Jordan Long <jordan.long222@gmail.com>
2016-07-09 15:22:13 -0400

    merge in saturday afternoon changes as a team

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -29,24 +29,67 @@ mainApp.controller('trendingController', ['$http', '$scope', function($http, $sc
         };
     });
 
-    // $scope.catchId = function (){
-    //   $scope.listing = this;
-    //   console.log(callOne.data.results.listing.listing_id);
-    //
-    // };
 
+    //second api call. triggered by button as of saturday, 7/9
+    var count = 0;
+
+    $('#loadMore').click(function(e) {
+      e.preventDefault();
+      count++;
+      lazyLoad(count);
+      console.log(count);
+    });
+
+
+    function lazyLoad(count){
+
+        switch (count) {
+
+
+            case 1:
+              console.log(count);
+                $http({
+                    method: 'GET',
+                    url: 'https://openapi.etsy.com/v2/listings/trending?limit=40&offset=40&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky'
+                }).then(function secondCall(second) {
+                    $scope.callTwo = second.data.results;
+                    console.log(second);
+                });
+                $scope.catchId = function(click){
+                  localStorage.setItem('listing_id',click);
+                  console.log(click);
+                };
+                break;
+            case 2:
+            console.log(count);
+
+                $http({
+                    method: 'GET',
+                    url: 'https://openapi.etsy.com/v2/listings/trending?limit=40&offset=80&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky'
+                }).then(function thirdCall(third) {
+                    $scope.callThree = third.data.results;
+                    console.log(third);
+                });
+                $scope.catchId = function(click){
+                  localStorage.setItem('listing_id',click);
+                  console.log(click);
+                };
+                break;
+            case 3:
+            console.log(count);
+
+                $http({
+                    method: 'GET',
+                    url: 'https://openapi.etsy.com/v2/listings/trending?limit=40&offset=120&includes=Shop,Images&api_key=9l86gozghbufirehm13mtdky'
+                }).then(function fourthCall(fourth) {
+                    $scope.callFour = fourth.data.results;
+                    console.log(fourth);
+                });
+                $scope.catchId = function(click){
+                  localStorage.setItem('listing_id',click);
+                  console.log(click);
+                };
+                break;
+        }
+}
 }]);
-
-
-//USED NG-IF INSTEAD......
-        // curCode will equal the results of all the currency_codes from the API call
-        // var currCode;
-        // var euro = 'EUR';
-        // var dolla = 'USD';
-        //
-        //
-        // if(currCode == euro){
-        //   return '&#128;';
-        // } else if (currCode == dolla){
-        //   return '&#36;';
-        // }
